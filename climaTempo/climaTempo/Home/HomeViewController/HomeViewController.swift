8a72450850fa409156ab0c8fec00cd445f41068b
Michael Bressiani <>
2024-01-08 00:00:35 -0300

    Inclusão de validação de TextField (validação de campo vazio) através de uma "Alert".

--- climaTempo/climaTempo/Home/HomeViewController/HomeViewController.swift
+++ climaTempo/climaTempo/Home/HomeViewController/HomeViewController.swift
@@ -13,14 +13,22 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var titleAppLabel: UILabel!
     @IBOutlet weak var descriptionAppLabel: UILabel!
     @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBOutlet weak var invalidLabel: UILabel!
     @IBOutlet weak var requestButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        elementsConfig()
+    }
+    
+    func elementsConfig() {
+        cityNameTextField.delegate = self
         titleAppLabel.text = "climaTempo"
         titleAppLabel.font = UIFont.systemFont(ofSize: 40, weight: .bold)
     
-        descriptionAppLabel.text = "Bem vindo ao climaTempo, aqui você pode consultar informações metereológicas de cidades, municipios e estados, de qualquer lugar do mundo, mas atenção certifique-se que a ortografia esteja correta."
+        descriptionAppLabel.text = "Bem vindo ao climaTempo, aqui você pode consultar informações metereológicas de cidades ou estados, de qualquer lugar do mundo, mas atenção certifique-se que a ortografia esteja correta."
         descriptionAppLabel.numberOfLines = 5
         descriptionAppLabel.textAlignment = .justified
         
@@ -30,6 +38,9 @@ class HomeViewController: UIViewController {
         requestButton.setTitleColor(UIColor.white, for: .normal)
         requestButton.backgroundColor = UIColor.orange
         requestButton.layer.cornerRadius = 5
+        
+        invalidLabel.text = ""
+        
     }
     
     @IBAction func tappedRequestButton(_ sender: UIButton) {
@@ -45,5 +56,55 @@ class HomeViewController: UIViewController {
 extension HomeViewController: DetailsViewModelProtocol {
     func transferTextFieldInfo() -> String {
         return cityNameTextField.text ?? "0"
+    }
+}
+
+extension HomeViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.blue.cgColor
+        textField.layer.cornerRadius = 5.0
+    }
+        
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        textField.layer.borderWidth = 1.0
+        textField.layer.cornerRadius = 5.0
+        if textField.hasText {
+            textField.layer.borderColor = UIColor.lightGray.cgColor
+                
+        } else {
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+            
+        // Verifique se o campo de email está vazio e não está em foco
+        if !textField.isFirstResponder {
+            if textField.text == "" {
+                textField.layer.borderColor = UIColor.red.cgColor
+                invalidLabel.font = UIFont.systemFont(ofSize: 15)
+                invalidLabel.textColor = UIColor.red
+                invalidLabel.text = "Preenchimento obrigatório"
+                
+                let alert: UIAlertController  = UIAlertController(title: "Preenchimento obrigatório", message: "", preferredStyle: .alert)
+                
+                
+                let action1: UIAlertAction = UIAlertAction(title: "Voltar", style: .default) {
+                    (action) in print("Default")
+                }
+                
+                alert.addAction(action1)
+                
+                self.present(alert, animated: true, completion: nil)
+                
+                
+            } else {
+                textField.layer.borderColor = UIColor.lightGray.cgColor
+                invalidLabel.text = ""
+            }
+        }
+    }
+        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
